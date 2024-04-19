c0ff6efee9d04d349360faf5c90e9d4739805b8f
Veliae <Veliae>
2024-04-19 09:13:47 -0400

    munchi

--- Example#2.py
+++ Example#2.py
@@ -7,6 +7,17 @@
 
 # Example #1:
 ## Part (a) Create a function that will check if a number is even or odd
+num = int(input("Enter a numebr: "))
+
+def even_odd(num: int,):
+    if num == 0:
+        print("0 is neither even nor odd")
+    elif num % 2 == 0:
+        print(f"{num} is even.")
+    else:
+        print(f"{num} Numer is odd. ")
+
 
 
 ## Part (b) Use your function by calling it
+even_odd(num)
--- Lesson.md
+++ Lesson.md
@@ -63,12 +63,14 @@ print(f"The area of the rectangle is: {area} cm^2")
 
 With a function:
 ```python
-
+def rect_area(length: float, width: float):
+    area = length * width
+    print(area)
 ```
 
 ### Part (b) Use your function by calling it
 ```
-
+rect_area(4, 5)
 ```
 
 <span style="color:red">
